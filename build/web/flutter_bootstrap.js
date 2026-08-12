e8e2c268647241ae9aa20f666aebbfcfbdb908ca
Sakshi <sakshibhagit@gmail.com>
2026-08-12 18:20:03 +0530

    Fix Netlify deployment

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3668532577" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4242472639" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
